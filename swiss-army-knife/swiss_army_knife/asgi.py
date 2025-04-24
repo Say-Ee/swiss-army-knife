7220611efc0773be76b9a1cd7152490f9551fb67
Sayee <sayeeepatil21@gmail.com>
2025-04-24 14:36:38 +0530

    Initial commit for swiss-army-knife project

---
+++ swiss-army-knife/swiss_army_knife/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for swiss_army_knife project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swiss_army_knife.settings')
+
+application = get_asgi_application()
